4af1592c6e7a99dcba614c63a7456ba995f5ee98
Yahya Demeriah <actuators.os@gmail.com>
2026-08-31 17:03:28 +0300

    Initial Commit

---
+++ Wrd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wrd
+//
+//  Created by Yahya on 18/03/1448 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wrd/WrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrdApp.swift
+//  Wrd
+//
+//  Created by Yahya on 18/03/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct WrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
